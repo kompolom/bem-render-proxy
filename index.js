6f4fb9542dcaf840fcf0d876a7651f839366817a
Evgeniy Baranov <master@kompolom.ru>
2016-11-12 12:34:36 +0300

    root commit

---
+++ index.js
@@ -0,0 +1,94 @@
+var express = require('express'),
+    fs = require('fs'),
+    app = express(),
+    bodyParser = require('body-parser'),
+    port = process.env.APP_PORT,
+    backendHost = process.env.BACKEND_HOST,
+    backendPort = process.env.BACKEND_PORT,
+    isSocket = isNaN(port),
+    http = require('http'),
+    FileStreamRotator = require('file-stream-rotator'),
+    morgan = require('morgan'),
+    logDir = process.env.LOGS_DIR || __dirname + '/logs',
+    Render = require('./render.js'),
+    DEBUG = process.env.APP_DEBUG,
+    render = Render.render;
+
+// ensure log directory exists
+fs.existsSync(logDir) || fs.mkdirSync(logDir);
+
+// create a rotating write stream
+var accessLogStream = FileStreamRotator.getStream({
+  date_format : 'YYYYMMDD',
+  filename : logDir + '/access-%DATE%.log',
+  frequency : 'daily',
+  verbose : false
+});
+
+morgan.token('render-time', function(req, res, digits){
+    if(!req._renderAt || !res._renderAt){
+    // missing request and/or response start time
+    return;
+  }
+
+  // calculate diff
+  var ms = (res._renderAt[0] - req._renderAt[0]) * 1e3 +
+    (res._renderAt[1] - req._renderAt[1]) * 1e-6;
+
+  // return truncated value
+  return ms.toFixed(digits === undefined? 3 : digits)
+});
+
+app
+    .disable('x-powered-by')
+    .disable('E-tag')
+    .use(morgan(':method :url :status - :response-time ms (render :render-time ms)', { stream : accessLogStream }))
+    .use(bodyParser.json());
+
+if(DEBUG){
+    app.use(morgan(':method :url :status - :response-time ms (render :render-time ms)'));
+}
+
+app.all('*', function(req, res) {
+    req.headers.accept = 'application/vnd.bem+json;' + req.headers.accept;
+    delete req.headers.host; // Удаляем оригинальный заголовок Host, чтобы web-server мог правильно определить vhost
+    var opts = {
+        method : req.method,
+        hostname : backendHost,
+        port : backendPort,
+        path : req.url,
+        headers : req.headers
+    },
+    clientReq = http.request(opts, function (backendMessage) {
+        var needRender = !!backendMessage.headers['x-render'] || backendMessage.headers['content-type'] === 'application/vnd.bem+json',
+            body = '';
+
+        if(!needRender){
+            res.writeHead(backendMessage.statusCode, backendMessage.headers);
+            backendMessage.on('data', (chunk) => {res.write(chunk);});
+            backendMessage.on('end', () => {res.end();});
+            return;
+        }
+
+        backendMessage.on('data', function (chunk) { body += chunk; });
+        backendMessage.on('end', function () {
+            res.status(backendMessage.statusCode);
+            render(req, res, JSON.parse(body));
+        });
+    });
+
+    clientReq.on('error', function(e){
+        res.sendStatus(502);
+        res.end();
+        console.log(e);
+    });
+
+    req.on('data', (chunk) => { clientReq.write(chunk); });
+    req.on('end', () => { clientReq.end(); });
+});
+isSocket && fs.existsSync(port) && fs.unlinkSync(port);
+
+app.listen(port, function() {
+    isSocket && fs.chmod(port, '0777');
+    console.log('render-proxy is listening on', this.address().port);
+});
---
+++ render.js
@@ -0,0 +1,96 @@
+'use strict';
+
+const path = require('path'),
+    isDev = process.env.NODE_ENV === 'development',
+    useCache = !isDev,
+    APP_ENV = process.env.APP_ENV,
+    DEBUG = process.env.APP_DEBUG,
+    DEFAULT_LANG = process.env.DEFAULT_LANG,
+    cacheTTL = process.env.CACHE_TTL,
+    staticRoot = process.env.STATIC_ROOT;
+
+var cache = {};
+
+function render(req, res, data, context) {
+    if(DEBUG && res.statusCode === 500 && APP_ENV === 'local') // FIXME remove this
+        return res.send('<pre>' + JSON.stringify(data, null, 4) + '</pre>');
+
+    var query = req.query,
+        user = req.user,
+        cacheKey = req.url + (context? JSON.stringify(context) : '') + (user? JSON.stringify(user) : ''),
+        cached = cache[cacheKey],
+
+        // Выбор бандла и платформы
+        platform = data.platform || 'desktop',
+        scope = data.bundle || 'shop',
+        view = data.page || 'home',
+        pathToBundle = path.resolve(platform + '.pages'),
+        bundleName = `page_${scope}_${view}`;
+
+        // Утанавливает url для статики
+        data.bundleUrl = path.join(`${staticRoot}${platform}.pages`, bundleName, bundleName);
+        data.lang  || (data.lang =  DEFAULT_LANG || 'ru');
+
+    recordRenderTime.call(req);
+
+    if(DEBUG && query.json) return res.send('<pre>' + JSON.stringify(data, null, 4) + '</pre>');
+
+    try {
+        var BEMTREE = require(path.join(pathToBundle, bundleName, `${bundleName}.${data.lang}.bemtree.js`)).BEMTREE,
+            BEMHTML = require(path.join(pathToBundle, bundleName, `${bundleName}.${data.lang}.bemhtml.js`)).BEMHTML;
+    } catch (err) {
+        console.error(err, err.stack);
+        return res.status(424).end(data.bundleUrl + ' error'); // Попытка подключить несуществующий бандл
+    }
+
+    if(useCache && cached && (new Date() - cached.timestamp < cacheTTL)) {
+        return res.send(cached.html);
+    }
+
+    var bemtreeCtx = {
+        block : 'root',
+        context : context,
+        // extend with data needed for all routes
+        data : Object.assign({}, {
+            url : req._parsedUrl
+        }, data)
+    };
+
+    try {
+        var bemjson = BEMTREE.apply(bemtreeCtx);
+    } catch(err) {
+        console.error('BEMTREE error', err.stack);
+        console.trace('server stack');
+        return res.status(500).send(err);
+    }
+
+    if(DEBUG && query.bemjson) return res.send('<pre>' + JSON.stringify(bemjson, null, 4) + '</pre>');
+
+    try {
+        var html = BEMHTML.apply(bemjson);
+    } catch(err) {
+        console.error('BEMHTML error', err.stack);
+        return res.status(500).send(err);
+    }
+
+    useCache && (cache[cacheKey] = {
+        timestamp : new Date(),
+        html : html
+    });
+
+    recordRenderTime.call(res);
+    res.send(html);
+}
+
+function dropCache() {
+    cache = {};
+}
+
+function recordRenderTime() {
+    this._renderAt = process.hrtime();
+}
+
+module.exports = {
+    render : render,
+    dropCache : dropCache
+};
