53ae0916ecdbfd0fd4390faf4dabbb7fd2ba6bbf
Evgeniy Baranov <master@kompolom.ru>
2018-02-05 17:11:50 +0300

    allow blacklist headers

--- bypassHeaders.js
+++ bypassHeaders.js
@@ -1,5 +1,11 @@
+const BLACKLISTED_HEADERS = {
+    'content-type' : true
+};
+
 module.exports = function bypassHeaders(from, to) {
     for (let headerName of Object.keys(from.headers)) {
+        if (BLACKLISTED_HEADERS[headerName]) continue;
+
         to.setHeader(headerName, from.headers[headerName]);
     }
 };
