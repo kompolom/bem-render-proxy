261e178287be59d03a04c17663c67a83d33975eb
Evgeniy Baranov <master@kompolom.ru>
2020-06-01 11:09:52 +0400

    mocha -> jest

--- test/bundle-scheme.test.js
+++ test/bundle-scheme.test.js
@@ -1,5 +1,4 @@
-const should = require('should'),
-    path = require('path'),
+const path = require('path'),
     BundleScheme = require('../bundle-scheme');
 
 describe('bundle-scheme', function() {
@@ -8,7 +7,7 @@ describe('bundle-scheme', function() {
     describe('{platform}.pages/page_{scope}_{view}', function() {
         var scheme = '{platform}.pages',
             page = 'page_{scope}_{view}';
-        before(function(){
+        beforeAll(function(){
             bundleScheme = new BundleScheme(scheme, page);
         });
 
@@ -16,21 +15,21 @@ describe('bundle-scheme', function() {
             bundleScheme.platform = 'desktop';
             bundleScheme.scope = 'shop';
             bundleScheme.view = 'index';
-            bundleScheme.bundle.should.be.equal('desktop.pages/page_shop_index');
+            expect(bundleScheme.bundle).toBe('desktop.pages/page_shop_index');
         });
 
         it('should return bundle path', function() {
             bundleScheme.platform = 'desktop';
             bundleScheme.scope = 'shop';
             bundleScheme.view = 'index';
-            bundleScheme.path.should.be.equal(path.resolve('desktop.pages', 'page_shop_index'));
+            expect(bundleScheme.path).toBe(path.resolve('desktop.pages', 'page_shop_index'));
         });
 
         it('should return bundle name', function() {
             bundleScheme.platform = 'desktop';
             bundleScheme.scope = 'shop';
             bundleScheme.view = 'index';
-            bundleScheme.name.should.be.equal('page_shop_index');
+            expect(bundleScheme.name).toBe('page_shop_index');
         });
 
         it('should change name', function() {
@@ -39,26 +38,26 @@ describe('bundle-scheme', function() {
             bundleScheme.view = 'index';
             bundleScheme.scope = 'admin';
             bundleScheme.view = 'documents';
-            bundleScheme.name.should.be.equal('page_admin_documents');
+            expect(bundleScheme.name).toBe('page_admin_documents');
         });
 
         it('should return file url', function() {
             bundleScheme.platform = 'desktop';
             bundleScheme.scope = 'shop';
             bundleScheme.view = 'index';
-            bundleScheme.getFileUrl('js', 'ru').should.be.equal('desktop.pages/page_shop_index/page_shop_index.ru.js');
+            expect(bundleScheme.getFileUrl('js', 'ru')).toBe('desktop.pages/page_shop_index/page_shop_index.ru.js');
         });
 
         it('static Root should have value', function() {
             let bs = new BundleScheme(scheme, page, 'http://abc.com/');
-            bs.staticRoot.should.be.equal('http://abc.com/')
+            expect(bs.staticRoot).toBe('http://abc.com/')
         });
     });
 
     describe('pages/{platform}/page_{scope}_{view}', function() {
         var scheme = 'pages/{platform}',
             page = 'page_{scope}_{view}';
-        before(function(){
+        beforeAll(function(){
             bundleScheme = new BundleScheme(scheme, page);
         });
 
@@ -66,7 +65,7 @@ describe('bundle-scheme', function() {
             bundleScheme.platform = 'desktop';
             bundleScheme.scope = 'shop';
             bundleScheme.view = 'index';
-            bundleScheme.baseUrl.should.be.equal('pages/desktop/page_shop_index/page_shop_index');
+            expect(bundleScheme.baseUrl).toBe('pages/desktop/page_shop_index/page_shop_index');
         });
     });
 });
