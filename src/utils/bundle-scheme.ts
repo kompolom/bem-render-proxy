65ac9f50dd41e0f372b81706e191490411ca55a7
Evgeniy Baranov <master@kompolom.ru>
2021-04-11 18:34:53 +0300

    change bundle-scheme format
    - add merge format to scheme
    - rename props
    - rename methods

--- src/utils/bundle-scheme.ts
+++ src/utils/bundle-scheme.ts
@@ -3,15 +3,35 @@ import path from "path";
 export class BundleScheme {
   public readonly bundleFormat: string;
   public readonly pageFormat: string;
-  public readonly staticRoot: string = "/";
+  public readonly mergeFormat: string;
+  public readonly baseUrl: string = "";
+  public readonly basePath: string = "";
+  /**
+   * Device platform
+   * @protected
+   */
   protected _platform: string;
   protected _scope: string;
   protected _view: string;
 
-  constructor(bundleFormat: string, pageFormat: string, staticRoot = "/") {
+  constructor({
+    bundleFormat,
+    pageFormat,
+    mergeFormat,
+    basePath = "",
+    baseUrl = "",
+  }: {
+    bundleFormat: string;
+    pageFormat: string;
+    mergeFormat?: string;
+    basePath?: string;
+    baseUrl?: string;
+  }) {
     this.bundleFormat = bundleFormat;
     this.pageFormat = pageFormat;
-    this.staticRoot = staticRoot;
+    this.mergeFormat = mergeFormat;
+    this.baseUrl = baseUrl;
+    this.basePath = basePath;
   }
 
   get platform(): string {
@@ -37,27 +57,43 @@ export class BundleScheme {
   get view(): string {
     return this._view;
   }
-  get baseUrl(): string {
-    return [this.bundle, this.name].join("/");
+
+  /**
+   * Базовый url merged-банлда
+   * @example http://example.com/deskop/auth
+   */
+  get mergedUrl(): string {
+    return this.baseUrl ? `${this.baseUrl}/${this.merge}` : this.merge;
+  }
+
+  /**
+   * Базовый url бандла
+   * @example http://example.com/desktop.pages/page_auth_login
+   */
+  get bundleUrl(): string {
+    return this.baseUrl ? `${this.baseUrl}/${this.bundle}` : this.bundle;
   }
 
   /**
    * Относительный путь до бандла
+   * @example desktop.pages/page_auth_login
    * @return {string}
    */
   get bundle(): string {
-    return [this.base, this.name].join("/");
-  }
-
-  /**
-   * Путь к бандлам
-   */
-  get base(): string {
-    return this.bundleFormat.replace("{platform}", this._platform);
+    return [this.bundleName, this.name].join("/");
   }
 
   /**
    * Имя бандла
+   * @example desktop.pages
+   */
+  get bundleName(): string {
+    return this.bundleFormat.replace("{platform}", this._platform);
+  }
+
+  /**
+   * Имя страницы
+   * @example page_auth_login
    */
   get name(): string {
     return this.pageFormat
@@ -65,11 +101,18 @@ export class BundleScheme {
       .replace("{view}", this._view);
   }
 
+  get merge(): string {
+    return this.mergeFormat
+      .replace("{platform}", this._platform)
+      .replace(/\{scope\}/g, this._scope)
+      .replace(/\{view\}/g, this._view);
+  }
+
   /**
    * Абсолютный путь к бандлу
    */
   get path(): string {
-    return path.resolve(this.bundle);
+    return path.resolve(this.basePath, this.bundle);
   }
 
   /**
@@ -79,16 +122,20 @@ export class BundleScheme {
    */
   getFile(ext: string, lang: string): string {
     lang = lang ? `.${lang}` : "";
-    return path.resolve(`${this.baseUrl}${lang}.${ext}`);
+    return path.resolve(this.path, `${this.name}${lang}.${ext}`);
   }
 
   /**
    * Получает url к файлу
    * @param ext - расширение
    * @param lang - локаль
+   * @param merged - использовать merged bundle
    */
-  getFileUrl(ext: string, lang: string): string {
+  getFileUrl(ext: string, lang?: string, merged?: boolean): string {
     lang = lang ? `.${lang}` : "";
-    return `${this.baseUrl}${lang}.${ext}`;
+    if (merged) {
+      return `${this.mergedUrl}${lang}.${ext}`;
+    }
+    return `${this.bundleUrl}/${this.name}${lang}.${ext}`;
   }
 }
